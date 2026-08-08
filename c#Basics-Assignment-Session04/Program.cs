06d1b1d2cf9fd1a50fc694471a8b4499de980ddf
ahmeddrabeii <ahma456ad@gmail.com>
2026-08-08 04:51:10 +0300

    Question 1

--- c#Basics-Assignment-Session04/Program.cs
+++ c#Basics-Assignment-Session04/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 1
+
+            //double[] values = { 25.5, 40.0, 33.75 };
+            //Console.WriteLine(values[1]);
+         
+            #endregion
         }
     }
 }
