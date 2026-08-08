78593d93bdaffc9c95db5d9c2c1f6eeb2a77de89
ahmeddrabeii <ahma456ad@gmail.com>
2026-08-08 05:53:36 +0300

    Question 2

--- c#Basics-Assignment-Session04/Program.cs
+++ c#Basics-Assignment-Session04/Program.cs
@@ -8,7 +8,15 @@
 
             //double[] values = { 25.5, 40.0, 33.75 };
             //Console.WriteLine(values[1]);
-         
+
+            #endregion
+
+
+            #region Question 2
+                                      //0       //1
+            //int[,] shelfCopies = { { 3, 5 }, { 1, 4 } };
+            //Console.WriteLine(shelfCopies[1, 0]);
+
             #endregion
         }
     }
