c7595b85c2df24d838fb7b96cdfcd2bd2403e1ae
ahmeddrabeii <ahma456ad@gmail.com>
2026-08-08 06:23:04 +0300

    Question 3

--- c#Basics-Assignment-Session04/Program.cs
+++ c#Basics-Assignment-Session04/Program.cs
@@ -3,7 +3,13 @@
     internal class Program
     {
         static void Main(string[] args)
+
         {
+           //PrintWelcomeMessage();
+        }
+
+
+        
             #region Question 1
 
             //double[] values = { 25.5, 40.0, 33.75 };
@@ -13,11 +19,21 @@
 
 
             #region Question 2
-                                      //0       //1
+            //0       //1
             //int[,] shelfCopies = { { 3, 5 }, { 1, 4 } };
             //Console.WriteLine(shelfCopies[1, 0]);
 
             #endregion
+
+
+            #region Question 3
+
+            //static void PrintWelcomeMessage()
+            //{
+            //    Console.WriteLine("Welcome to the Library!");
+            //}
+
+            #endregion
         }
     }
-}
+
